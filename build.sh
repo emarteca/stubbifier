f1a36cdc937c66819a5e63bb0b39cd5c7477ed05
Ellen Arteca <ellen.arteca@gmail.com>
2022-02-09 15:34:25 -0500

    hardcoding version of codeQL to be downloaded

--- build.sh
+++ build.sh
@@ -14,10 +14,12 @@ rm -r local_mount
 
 mkdir -p /home/codeql_home
 
-mv /home/stubbifier/codeql-linux64.zip /home/codeql_home/ 
 cd /home/codeql_home
+curl -L -o codeql-linux64.zip https://github.com/github/codeql-cli-binaries/releases/download/v2.3.4/codeql-linux64.zip
 unzip codeql-linux64.zip 
-git clone https://github.com/github/codeql.git codeql-repo
+# clone stable version
+git clone https://github.com/github/codeql.git --branch v1.26.0 codeql-repo
+
 # replace the built-in QL Modules definitions with our own, that will find modules in node_modules
 mv /home/stubbifier/Modules.qll codeql-repo/javascript/ql/src/semmle/javascript/
 
